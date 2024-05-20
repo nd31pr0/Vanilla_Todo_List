de4d88ea07d18c7253194208cb9c646926a9cbd3
Ndzi Nfor <ndzinfor@gmail.com>
2024-05-20 19:34:06 +0100

    create project module

---
+++ src/modules/project.js
@@ -0,0 +1,12 @@
+export class Project {
+    constructor(name){
+        this.name = name;
+        this.todos = [];
+    }
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
+    removeTodo(todoTitle){
+        this.todos = this.todos.filter(todo => todo.title !== todoTitle);
+    }
+}
